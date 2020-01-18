4b961734793c3b8752c1d33369424af1a90526c3
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-01-18 19:39:03 +0000

    jQuery for menu

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	$('#pagepiling').pagepiling({
+        direction: 'vertical',
+        sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', '#03a0b0', '#ff6f69'],
+        anchors: ['home', 'about', 'port', 'service', 'contact'],
+        menu: '#myMenu',
+        navigation: {
+            'textColor': '#000',
+            'bulletsColor': '#000',
+            'position': 'right',
+            'tooltips': ['Home', 'About', 'Portfolio', 'Services', 'Contact']
+        },
+        });
+
+    $("nav ul li a").hover(function(){
+        $(this).css("color", "red");
+        $(this).css("font-size", "30px");
+        }, function(){
+         $(this).css("color", "black");
+        $(this).css("font-size", "20px");
+    });
+
+    $(".callToAction").hover(function(){
+        $(this).css("background", "grey");
+        }, function(){
+         $(this).css("background", "red");
+    });
+});
